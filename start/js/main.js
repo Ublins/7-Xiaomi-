f7661454be6271ff7b650416a05fd29be7b6a98e
Ublins <ooublina@gmail.com>
2023-02-17 16:30:26 +0200

    2 repositori

--- start/js/main.js
+++ start/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('.bike-slider') .slick({
+    $('.bike-slider, .slider__items') .slick({
         arrows: false,
         dots: true,
         fade:true,
@@ -7,3 +7,139 @@ $(function () {
         autoplaySpeed: 2000,
     })
 })
+
+
+
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+
+
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''), 
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log (personalMovieDB);
+
+
+
+// console.log( NaN || 2 || undefined ); + 
+// console.log( NaN && 2 && undefined ); + 
+// console.log( 1 && 2 && 3 ); +
+// console.log( !1 && 2 || !3 ); +- 
+// console.log( 25 || null && !3 );+
+// console.log( NaN || null || !3 || undefined || 5);+
+// console.log( NaN || null && !3 && undefined || 5); + 
+// console.log( 5 === 5 && 3 > 1 || 5); +/- 
+
+
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// for (let i = 5; i <=10; i++) {
+//     console.log (i);
+    
+// }
+
+// for (let i = 20; i >=10; i--) {
+//     if (i === 12) {
+//         break
+//     };
+//     console.log (i);
+    
+// }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i%2 === 0) {
+//         console.log (i)
+//     }
+// }
+ 
+
+// let num = 2;
+// while (num < 17) {
+//     if (num % 2 === 0) {
+//         num++;
+//         continue;
+//     }  else {
+//         console.log (num);
+//     }
+//     num++
+// }
+
+
+// let i = 2;
+
+// while (i <= 16) {
+//     if (i % 2 === 0) {
+//         i++;
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// const arrayOfNumbers = [];
+
+// for (let i = 5; i < 11; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
+
+// console.log(arrayOfNumbers);
+// return arrayOfNumbers;
+
+
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+
